63f6ccc24e4d234255b76cb56bda5271860f6c90
Douglas Silva <thedouglasilva@gmail.com>
2023-05-07 10:14:31 -0300

    alteração formulário

--- script.js
+++ script.js
@@ -10,7 +10,8 @@ function cadastrar() {
     let faltas = document.getElementById('faltas').value;
     let notaum = document.getElementById('notaum').value;
     let notadois = document.getElementById('notadois').value;
-    if (nome == "" || curso == "" || periodo == "") {
+    let media = (parseInt(notaum) + parseInt(notadois))/2;
+    if (nome == "" || curso == "" || periodo == "" || faltas == "" || notaum == "" || notadois == "") {
         alert("Preencha todos os campos!");
         return;
     }
@@ -23,7 +24,8 @@ function cadastrar() {
         <td id="faltas_aluno">${faltas}</td>
         <td id="notaum_aluno">${notaum}</td>
         <td id="notadois_aluno">${notadois}</td>
-        <td id="media_aluno">${(parseInt(notaum) + parseInt(notadois))/2}</td>
+        <td id="media_aluno">${media}</td>
+        <td id="situacao_aluno">${situacao(faltas, media)}</td>
         <td> <button id="alterar" onclick=alterar(${contador})><img src="images/editar.png"></button> </td>
         <td> <button id="excluir" onclick=excluir(${contador})><img src="images/excluir.png"></button> </td>
     </tr>`
@@ -49,20 +51,38 @@ function alterar(id) {
         <label for="nome">Período:</label>
         <input id="periodo_alteracao" type="number" required max="10" min="1" />
     </div>
+    <div class="colocar_notas">
+        <label for="nome">1º Nota:</label>
+        <input id="notaum_alteracao" type="number" required max="10" min="0" />
+        <label for="nome">2º Nota:</label>
+        <input id="notadois_alteracao" type="number" required max="10" min="0" />
+        <label for="nome">Faltas:</label>
+        <input id="faltas_alteracao" type="number" required max="72" min="0" />
     </div>
-    <button class="cadastrar" onclick=alteracao(${id})>Confirmar a alteração</button>`
+    <div class="botton-cad">
+        <button class="cadastrar" onclick=alteracao(${id})>Confirmar a alteração</button>
+    </div>
+    </div>`
 }
 
 function alteracao(id) {
     nome_novo = document.getElementById('nome_alteracao').value;
     curso_novo = document.getElementById('curso_alteracao').value;
     periodo_novo = document.getElementById('periodo_alteracao').value;
-    console.log(nome_novo)
+    notaum_nova = document.getElementById('notaum_alteracao').value;
+    notadois_nova = document.getElementById('notadois_alteracao').value;
+    faltas_nova = document.getElementById('faltas_alteracao').value;
+    media_nova = (parseInt(notaum_nova) + parseInt(notadois_nova))/2;
     document.querySelector(`#cliente_${id}`).innerHTML = `
     <tr id="cliente_${id}">
         <td>${nome_novo}</td>    
         <td>${curso_novo}</td>
         <td>${periodo_novo}º Período</td>
+        <td id="faltas_aluno">${faltas_nova}</td>
+        <td id="notaum_aluno">${notaum_nova}</td>
+        <td id="notadois_aluno">${notadois_nova}</td>
+        <td id="media_aluno">${media_nova}</td>
+        <td id="situacao_aluno">${situacao(faltas_nova, media_nova)}</td>
         <td> <button id="alterar" onclick=alterar(${id})><img src="images/editar.png"></button> </td>
         <td> <button id="excluir" onclick=excluir(${id})><img src="images/excluir.png"></button> </td>
     </tr>`
@@ -77,7 +97,6 @@ search.addEventListener('input', () => {
     if (termoDeBusca.length > 0) {
       for (var i = -1; i <= nomes.length; i++) {
           if (termoDeBusca == nomes[i+1].slice(0, termoDeBusca.length)) {
-              console.log("SIm")
               let elemento = document.getElementById(`cliente_${i+1}`);
               elemento.style.display = '';
           } else {
@@ -101,4 +120,14 @@ function limpar_inputs() {
     document.getElementById('nome').value = '';
     document.getElementById('curso').value = '';
     document.getElementById('periodo').value = '';
+}
+
+function situacao(faltas, media) {
+    if (faltas > 18) {
+        return 'Reprovado por falta'
+    } else if (media < 7) {
+        return 'Recuperação final'
+    } else {
+        return 'Aprovado por média'
+    }
 }
