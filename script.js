ff5f491f8e03e5a8a5cb1441d980fc4a6cd1e42b
Douglas Silva <thedouglasilva@gmail.com>
2023-05-06 21:00:53 -0300

    add 'editar' and 'excluir'

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 contador = 0
 var botao = document.getElementById("alterar");
 var search = document.querySelector('#busca');
-nomes = []
+nomes = ["Douglas Araújo Silva"]
 
 function cadastrar() {
     nome = document.getElementById('nome').value;
     curso = document.getElementById('curso').value;
     periodo = document.getElementById('periodo').value;
+    if (nome == "" || curso == "" || periodo == "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
     contador++;
     document.querySelector('#lista-nomes').innerHTML += `
     <tr id="cliente_${contador}">
         <td id="nome_aluno">${nome}</td>    
         <td id="curso_aluno">${curso}</td>
         <td id="periodo_aluno">${periodo}</td>
-        <td <button id="alterar" onclick=alterar(${contador})>Alterar</button> </td>
-        <td <button id="excluir" onclick=excluir(${contador})>Excluir</button> </td>
+        <td> <button id="alterar" onclick=alterar(${contador})><img src="images/editar.png"></button> </td>
+        <td> <button id="excluir" onclick=excluir(${contador})><img src="images/excluir.png"></button> </td>
     </tr>`
     nomes.push(nome)
-    console.log(nomes)
 }
 
 function excluir(id) {
@@ -26,14 +29,14 @@ function excluir(id) {
 
 function alterar(id) {
     document.querySelector('.formulario_alteracao').innerHTML = `<div class="formulario">
-    <label for="nome">Nome Completo:</label>
-    <input type="text" id="nome_alteracao" placeholder="Digite o seu nome" required> <br><br>
+    <label for="nome">Nome:</label>
+    <input type="text" id="nome_alteracao" placeholder="Digite o seu nome" required value="${nomes[id]}"> <br><br>
     <label for="nome">Curso:</label>
     <input type="text" id="curso_alteracao" placeholder="Digite o seu curso" required> <br><br>
     <label for="nome">Período:</label>
     <input type="text" id="periodo_alteracao" placeholder="Digite o seu período" required> <br><br>
     </div>
-    <button class="cadastrar" onclick=alteracao(${id})>Faça a alteração</button>`
+    <button class="cadastrar" onclick=alteracao(${id})>Confirmar a alteração</button>`
 }
 
 function alteracao(id) {
@@ -46,21 +49,20 @@ function alteracao(id) {
         <td>${nome_novo}</td>    
         <td>${curso_novo}</td>
         <td>${periodo_novo}</td>
-        <td <button id="alterar" onclick=alterar(${id})>Alterar</button> </td>
-        <td <button id="excluir" onclick=excluir(${id})>Excluir</button> </td>
+        <td> <button id="alterar" onclick=alterar(${id})><img src="images/editar.png"></button> </td>
+        <td> <button id="excluir" onclick=excluir(${id})><img src="images/excluir.png"></button> </td>
     </tr>`
-    nomes[id-1] = nome_novo
+    nomes[id] = nome_novo
     console.log(nomes)
     document.querySelector('.formulario_alteracao').innerHTML = ''
 }
-
 
 search.addEventListener('input', () => {
     const termoDeBusca = search.value;
     console.log(termoDeBusca)
     if (termoDeBusca.length > 0) {
       for (var i = 0; i <= nomes.length; i++) {
-          if (termoDeBusca == nomes[i].slice(0, termoDeBusca.length)) {
+          if (termoDeBusca == nomes[i+1].slice(0, termoDeBusca.length)) {
               console.log("SIm")
               let elemento = document.getElementById(`cliente_${i+1}`);
               elemento.style.display = '';
@@ -70,9 +72,13 @@ search.addEventListener('input', () => {
           }
         }
     } else { 
-      for (var i = 1; i < nomes.length; i++) {
-          let elemento = document.getElementById(`cliente_${i}`);
+      for (var i = 0; i < nomes.length; i++) {
+          let elemento = document.getElementById(`cliente_${i+1}`);
               elemento.style.display = '';
       }
     }
   });
+
+document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+});
