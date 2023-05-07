8a410ec2a2466044739be30dd06574b4ca74aa48
Douglas Silva <thedouglasilva@gmail.com>
2023-05-07 13:48:36 -0300

    alteração na função cadastrar

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let botao = document.getElementById("alterar");
 let search = document.querySelector('#busca');
 nomes = ["Douglas Araújo Silva"]
 
-function cadastrar() {
+function cadastrar(comando, id) {
     let nome = document.getElementById('nome').value;
     let curso = document.getElementById('curso').value;
     let periodo = document.getElementById('periodo').value;
@@ -11,13 +11,18 @@ function cadastrar() {
     let notaum = document.getElementById('notaum').value;
     let notadois = document.getElementById('notadois').value;
     let media = (parseInt(notaum) + parseInt(notadois))/2;
-    if (nome == "" || curso == "" || periodo == "" || faltas == "" || notaum == "" || notadois == "") {
-        alert("Preencha todos os campos!");
-        return;
+    if (comando != "alterar") {
+        contador++;
+        document.querySelector(`#lista-nomes`).innerHTML += `<tr id="cliente_${contador}">
+        </tr>`
+        id = contador
+        nomes.push(nome);
     }
-    contador++;
-    document.querySelector('#lista-nomes').innerHTML += `
-    <tr id="cliente_${contador}">
+    if (comando == "alterar") {
+        document.querySelector('.botton-cad').innerHTML = `<button id="cadastrar_st" onclick='cadastrar("cadastro")'>Cadastrar</button> <br><br>`;
+        nomes[id] = nome
+    }
+    document.querySelector(`#cliente_${id}`).innerHTML = `
         <td id="nome_aluno">${nome}</td>    
         <td id="curso_aluno">${curso}</td>
         <td id="periodo_aluno">${periodo}º Período</td>
@@ -26,69 +31,18 @@ function cadastrar() {
         <td id="notadois_aluno">${notadois}</td>
         <td id="media_aluno">${media}</td>
         <td id="situacao_aluno">${situacao(faltas, media)}</td>
-        <td> <button id="alterar" onclick=alterar(${contador})><img src="images/editar.png"></button> </td>
-        <td> <button id="excluir" onclick=excluir(${contador})><img src="images/excluir.png"></button> </td>
-    </tr>`
-    nomes.push(nome);
+        <td> <button id="alterar" onclick='formulario_alteracao(${id})'><img src="images/editar.png"></button> </td>
+        <td> <button id="excluir" onclick=excluir(${id})><img src="images/excluir.png"></button> </td>`
+    
     limpar_inputs();
+}
+
+function formulario_alteracao(id) {
+    document.querySelector('.botton-cad').innerHTML = `<button id="alterar_st" onclick='cadastrar("alterar", ${id})'>Alterar</button>`;
 }
 
 function excluir(id) {
     document.querySelector(`#cliente_${id}`).innerHTML = ''
-}
-
-function alterar(id) {
-    document.querySelector('.formulario_alteracao').innerHTML = `<div class="formulario">
-    <label for="nome">Nome:</label>
-    <input type="text" id="nome_alteracao" placeholder="Digite o seu nome" required value="${nomes[id]}"> <br><br>
-    <label for="nome">Curso:</label>
-    <select name="curso" id="curso_alteracao">
-        <option value="Sistemas de Informação">Sistemas de Informação</option>
-        <option value="Ciência da Computação">Ciência da Computação</option>
-        <option value="Engenharia da Computação">Engenharia da Computação</option>
-    </select>
-    <div class="colocar_periodo">
-        <label for="nome">Período:</label>
-        <input id="periodo_alteracao" type="number" required max="10" min="1" />
-    </div>
-    <div class="colocar_notas">
-        <label for="nome">1º Nota:</label>
-        <input id="notaum_alteracao" type="number" required max="10" min="0" />
-        <label for="nome">2º Nota:</label>
-        <input id="notadois_alteracao" type="number" required max="10" min="0" />
-        <label for="nome">Faltas:</label>
-        <input id="faltas_alteracao" type="number" required max="72" min="0" />
-    </div>
-    <div class="botton-cad">
-        <button class="cadastrar" onclick=alteracao(${id})>Confirmar a alteração</button>
-    </div>
-    </div>`
-}
-
-function alteracao(id) {
-    nome_novo = document.getElementById('nome_alteracao').value;
-    curso_novo = document.getElementById('curso_alteracao').value;
-    periodo_novo = document.getElementById('periodo_alteracao').value;
-    notaum_nova = document.getElementById('notaum_alteracao').value;
-    notadois_nova = document.getElementById('notadois_alteracao').value;
-    faltas_nova = document.getElementById('faltas_alteracao').value;
-    media_nova = (parseInt(notaum_nova) + parseInt(notadois_nova))/2;
-    document.querySelector(`#cliente_${id}`).innerHTML = `
-    <tr id="cliente_${id}">
-        <td>${nome_novo}</td>    
-        <td>${curso_novo}</td>
-        <td>${periodo_novo}º Período</td>
-        <td id="faltas_aluno">${faltas_nova}</td>
-        <td id="notaum_aluno">${notaum_nova}</td>
-        <td id="notadois_aluno">${notadois_nova}</td>
-        <td id="media_aluno">${media_nova}</td>
-        <td id="situacao_aluno">${situacao(faltas_nova, media_nova)}</td>
-        <td> <button id="alterar" onclick=alterar(${id})><img src="images/editar.png"></button> </td>
-        <td> <button id="excluir" onclick=excluir(${id})><img src="images/excluir.png"></button> </td>
-    </tr>`
-    nomes[id] = nome_novo
-    console.log(nomes)
-    document.querySelector('.formulario_alteracao').innerHTML = ''
 }
 
 search.addEventListener('input', () => {
