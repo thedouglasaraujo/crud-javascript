3b717bd313b32a57503b7abf0d24c7a7996eb998
Douglas Silva <thedouglasilva@gmail.com>
2023-05-08 10:07:07 -0300

    alterando formatação tabela

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let contador = 0
+let contador = 0;
 let botao = document.getElementById("alterar");
 let search = document.querySelector('#busca');
-nomes = ["Douglas Araújo Silva"]
+nomes = ["Douglas Araújo Silva"];
 
 function cadastrar(comando, id) {
     let nome = document.getElementById('nome').value;
@@ -11,19 +11,27 @@ function cadastrar(comando, id) {
     let notaum = document.getElementById('notaum').value;
     let notadois = document.getElementById('notadois').value;
     let media = (parseInt(notaum) + parseInt(notadois)) / 2;
-    if (comando != "alterar") {
+
+    if (nome == "" || curso == "" || periodo == "" || faltas == "" || notaum == "" || notadois == "") {
+        alert("Preencha todos os campos!");
+        return;
+    }
+
+    if (comando == "cadastro") {
         contador++;
         document.querySelector(`#lista-nomes`).innerHTML += `<tr id="cliente_${contador}">
         </tr>`
-        id = contador
+        id = contador;
         nomes.push(nome);
     }
+
     if (comando == "alterar") {
-        document.querySelector('.botton-cad').innerHTML = `<button id="cadastrar_st" onclick='cadastrar("cadastro")'>Cadastrar</button> <br><br>`;
+        document.querySelector('#botton-cad').innerHTML = `<button id="cadastrar_st" onclick='cadastrar("cadastro")'>Cadastrar</button> <br><br>`;
         let elemento = document.getElementById('sombra');
         elemento.style.boxShadow = '';
         nomes[id] = nome;
     }
+
     document.querySelector(`#cliente_${id}`).innerHTML = `
         <td id="nome_aluno">${nome}</td>    
         <td id="curso_aluno">${curso}</td>
@@ -35,23 +43,23 @@ function cadastrar(comando, id) {
         <td id="situacao_aluno">${situacao(faltas, media)}</td>
         <td> <button id="alterar" onclick='formulario_alteracao(${id}, ${JSON.stringify(nome)})'><img src="images/editar.png"></button> </td>
         <td> <button id="excluir" onclick=excluir(${id})><img src="images/excluir.png"></button> </td>`
+
     limpar_inputs();
-}
+};
 
 function formulario_alteracao(id, nome) {
     let elemento = document.getElementById('sombra');
     elemento.style.boxShadow = '0px 0px 26px #990202';
     document.getElementById('nome').value = nome;
-    document.querySelector('.botton-cad').innerHTML = `<button id="alterar_st" onclick='cadastrar("alterar", ${id})'>Alterar</button>`;
-}
+    document.querySelector('#botton-cad').innerHTML = `<button id="alterar_st" onclick='cadastrar("alterar", ${id})'>Alterar</button>`;
+};
 
 function excluir(id) {
     document.querySelector(`#cliente_${id}`).innerHTML = ''
-}
+};
 
 search.addEventListener('input', () => {
     const termoDeBusca = search.value;
-    console.log(termoDeBusca)
     if (termoDeBusca.length > 0) {
         for (var i = -1; i <= nomes.length; i++) {
             if (termoDeBusca == nomes[i + 1].slice(0, termoDeBusca.length)) {
@@ -81,17 +89,16 @@ function limpar_inputs() {
     document.getElementById('notaum').value = '';
     document.getElementById('notadois').value = '';
     document.getElementById('faltas').value = '';
-    
-}
+};
 
 function situacao(faltas, media) {
     if (faltas > 18) {
-        return 'Reprovado por falta'
-    } else if (3 < media < 7) {
-        return 'Recuperação final'
+        return 'Reprovado por falta';
+    } else if (media < 7 && media > 3) {
+        return 'Recuperação final';
     } else if (media < 3) {
-        return 'Reprovado'
+        return 'Reprovado';
     } else {
-        return 'Aprovado por média'
+        return 'Aprovado por média';
     }
-}
+};
